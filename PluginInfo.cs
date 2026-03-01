f1ddbd2470621fa99f25b991486fcde1a30b4b00
liamo <liamo@DESKTOP-89FE52V>
2026-03-01 15:42:00 -0800

    Add project files.

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace StupidTemplate
+{
+    public class PluginInfo
+    {
+        public const string GUID = "org.rexes.gorillatag.rexesmenu";
+        public const string Name = "rexes menu";
+        public const string Description = "cool menu made by tcmrexvr";
+        public const string Version = "1.0.0";
+    }
+}
